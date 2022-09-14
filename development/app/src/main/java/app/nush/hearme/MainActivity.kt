b89a93d4708d4bb15501949d41496b42a336acad
jedlimlx <jedlimlx@outlook.com>
2022-09-14 22:33:00 +0800

    Make settings screen actually functional by hiding / revealing the nodes based on the settings

--- development/app/src/main/java/app/nush/hearme/MainActivity.kt
+++ development/app/src/main/java/app/nush/hearme/MainActivity.kt
@@ -1,5 +1,6 @@
 package app.nush.hearme
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -9,6 +10,8 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -16,7 +19,10 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.datastore.core.DataStore
+import androidx.datastore.dataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -28,6 +34,9 @@ import app.nush.hearme.ui.TTSField
 import app.nush.hearme.ui.theme.HearMeTheme
 import com.jamal.composeprefs.ui.PrefsScreen
 import com.jamal.composeprefs.ui.prefs.SwitchPref
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.map
 
 val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 
@@ -48,6 +57,8 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Content() {
     val navController = rememberNavController()
+    val useTTS = remember { mutableStateOf(true) }
+    val useLipSync = remember { mutableStateOf(true) }
 
     Column {
         CallBar()
@@ -56,15 +67,17 @@ fun Content() {
                 Column(
                     modifier = Modifier.padding(10.dp).wrapContentWidth()
                 ) {
-
                     Divider()
 
-                    TTSField()
+                    if (useTTS.value) {
+                        TTSField()
 
-                    Divider()
+                        Divider()
+                    }
 
                     Row {
-                        LipSync()
+                        if (useLipSync.value)
+                            LipSync()
 
                         SmallSettings { navController.navigate("settings") }
                     }
@@ -78,8 +91,26 @@ fun Content() {
                             //prefsItem { TextPref(title = "Settings", summary = "Customise your experience :D") }
 
                             prefsGroup("Settings") {
-                                prefsItem { SwitchPref(key = "tts", title = "Use Text to Speech") }
-                                prefsItem { SwitchPref(key = "lip_sync", title = "Use Deepfake Lip Sync") }
+                                prefsItem {
+                                    SwitchPref(
+                                        key = "tts",
+                                        title = "Use Text to Speech",
+                                        defaultChecked = true,
+                                        onCheckedChange = {
+                                            useTTS.value = it
+                                        }
+                                    )
+                                }
+                                prefsItem {
+                                    SwitchPref(
+                                        key = "lip_sync",
+                                        title = "Use Deepfake Lip Sync",
+                                        defaultChecked = true,
+                                        onCheckedChange = {
+                                            useLipSync.value = it
+                                        }
+                                    )
+                                }
                             }
                         }
                     }
